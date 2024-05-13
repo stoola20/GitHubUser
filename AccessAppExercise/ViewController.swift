8fe32b87312027c173a50e692c008b42f77d211b
Jesse Chen <jesse.chen@gomaji.com>
2024-05-13 14:58:20 +0800

    Initial Commit

---
+++ AccessAppExercise/ViewController.swift
@@ -0,0 +1,20 @@
+////
+//  ViewController.swift
+//  AccessAppExercise
+//
+//  Created by 1710 on 2024/5/13.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
